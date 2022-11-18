6fae040b02e9442e508a6026c38ccdf02a8909a7
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-18 15:39:31 +0100

    Created js script for psw generation

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let userName = prompt("Inserisci il nuo nome")
 let userSurname = prompt("Inserisci il tuo cognome")
 let userFavouriteColor = prompt("Inserisci il tuo colore preferito")
+let newPsw = userName + userSurname + userFavouriteColor
+
+document.getElementById('psw-output').innerHTML = newPsw
