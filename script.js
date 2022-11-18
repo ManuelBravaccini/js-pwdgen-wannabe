2f932d8459fee492f8e03d9b2a5e6bbdbcee19d4
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-18 17:41:54 +0100

    Added number variable in script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let userName = prompt("Inserisci il nuo nome")
 let userSurname = prompt("Inserisci il tuo cognome")
 let userFavouriteColor = prompt("Inserisci il tuo colore preferito")
-let newPsw = userName + userSurname + userFavouriteColor
+let number = ('22')
+let newPsw = userName + userSurname + userFavouriteColor + number
 
 document.getElementById('psw-output').innerHTML = newPsw
